3f2f6eadc663cd46463dcc57a642beba8d9e60e9
MOHAMAD MIRZA <19025376@myrp.edu.sg>
2021-01-12 12:05:30 +0800

    First Commit

---
+++ getStatistics.php
@@ -0,0 +1,23 @@
+<?php
+
+include "dbFunctions.php"; 
+
+// SQL query returns multiple database records.
+$query = "SELECT * FROM statistics order by id"; 
+$result = mysqli_query($link, $query);
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $statistics[] = $row;
+}
+mysqli_close($link);
+
+echo json_encode($statistics);
+?>
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
